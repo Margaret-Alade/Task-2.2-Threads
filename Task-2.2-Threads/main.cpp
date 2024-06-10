b8e7ee04e6a127d6ca4bff6588e991d4bd19497a
Margaret Alade <alade.margaret@yandex.ru>
2024-06-10 21:12:47 +0300

    Initial commit

--- Task-2.2-Threads/main.cpp
+++ Task-2.2-Threads/main.cpp
@@ -6,9 +6,38 @@
 //
 
 #include <iostream>
+#include <thread>
+#include <mutex>
+#include <vector>
+
+
+std::mutex m;
+
+void func(int num) {
+    std::lock_guard lk(m);
+    num += 4;
+    num -= 3;
+    num *= 15;
+}
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::vector<std::thread> v;
+    for (int i = 0; i < 4; ++i) {
+        v.push_back(std::thread(func, i + 1));
+    }
+    for (auto &el : v) {
+        el.join();
+    }
+    
+    std::cout << "#\t\t" << "id\t\t" <<  "Progress bar" << "\t" << "Time" << "\n";
+    int i = 0;
+    
+    for (auto &el : v) {
+        std::cout << i << "\t\t" << el.get_id() << "\n";
+        i++;
+    }
+    
     return 0;
 }
